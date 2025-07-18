9cb171bddb12c3fc7621e977e7f1e99d1a989462
Pedro <2022302914@aluno.erechim.ifrs.edu.br>
2025-07-17 21:54:07 -0300

    chore(cors): 🔧 Configura CORS para permitir requisições do frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,14 @@
-# Be sure to restart your server when you modify this file.
+# config/initializers/cors.rb
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # Permite que o seu frontend React (em localhost:5173) faça requisições
+    origins "http://localhost:5173"
 
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      # IMPORTANTE: Expor o header 'Authorization' para que o frontend possa lê-lo
+      expose: ['Authorization']
+  end
+end
