d36b87316bb0ded8b520ca18eeac99ba855ed53b
Pedro Henrique Urio dos Santos <106077640+pedrourio@users.noreply.github.com>
2025-07-21 13:18:43 -0300

    "chore(aws): 🔧 Prepara a aplicação para o deploy no Elastic Beanstalk"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
+  config.hosts << ".elasticbeanstalk.com"
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
